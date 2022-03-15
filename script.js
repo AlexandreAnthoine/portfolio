72f22f1b07453727842d6a64243c56b3b6637e89
Alexandre Anthoine <71062085+AlexandreAnthoine@users.noreply.github.com>
2022-03-15 15:38:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle"),
+      searchBtn = body.querySelector(".search-box"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
+
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+});
+
+const btnCopy = document.querySelector('.btn-3');
+const txtCopy = document.querySelector('#id-discord');
+
+btnCopy.addEventListener('click', () => {
+    navigator.clipboard.writeText(txtCopy.innerText);
+})
+
